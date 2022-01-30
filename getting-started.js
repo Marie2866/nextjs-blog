8f796a74c232091d14151c5b18f1acfd987e8e06
Marie2866 <94830782+Marie2866@users.noreply.github.com>
2022-01-30 17:14:02 +0100

    Update getting-started.js

--- getting-started.js
+++ getting-started.js
@@ -3,5 +3,5 @@ const mongoose = require("mongoose");
 main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect("mongodb://localhost:27017/test");
+  await mongoose.connect("mongodb://localhost:27017/projetMP");
 }
