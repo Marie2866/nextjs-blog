1976b1d222cf941d7bacdfdc3dce8300f2f460c8
Marie2866 <94830782+Marie2866@users.noreply.github.com>
2022-02-11 10:43:11 +0100

    Update apiblog.js

--- apiblog.js
+++ apiblog.js
@@ -1,33 +1,12 @@
-//L'application requiert l'utilisation du module Express.
-//La variable express nous permettra d'utiliser les fonctionnalités du module Express.
-const express = require("express");
+//Import the mongoose module
+var mongoose = require('mongoose');
 
-const app = express();
-app.use(express.json());
-var express = require("express");
+//Set up default mongoose connection
+var mongoDB = 'mongodb://localhost:27017/test';
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
 
-// Nous définissons ici les paramètres du serveur.
-
-var hostname = "localhost";
-var port = 3000;
-
-// La variable mongoose nous permettra d'utiliser les fonctionnalités du module mongoose.
-var mongoose = require("mongoose");
-// Ces options sont recommandées par mLab pour une connexion à la base
-var options = {
-  server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
-  replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
-};
-
-//URL de notre base
-var urlmongo =
-  "mongodb://localhost:27017/projetMP";
-
-// Nous connectons l'API à notre base de données
-mongoose.connect(urlmongo, options);
-
+//Get the default connection
 var db = mongoose.connection;
-db.on("error", console.error.bind(console, "Erreur lors de la connexion"));
-db.once("open", function () {
-  console.log("Connexion à la base OK");
-});
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
