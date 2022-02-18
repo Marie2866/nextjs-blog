1ebc5345389b162b427427a512cc803a9742fe1a
Marie2866 <94830782+Marie2866@users.noreply.github.com>
2022-02-18 09:25:57 +0100

    Add getting-started.js

--- getting-started.js
+++ getting-started.js
@@ -1,12 +1,33 @@
-//Import the mongoose module
-var mongoose = require('mongoose');
+//L'application requiert l'utilisation du module Express.
+//La variable express nous permettra d'utiliser les fonctionnalités du module Express.
+var express = require("express");
 
-//Set up default mongoose connection
-var mongoDB = 'mongodb://localhost:27017/test';
-mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+// Nous définissons ici les paramètres du serveur.
+var hostname = "localhost";
+var port = 3000;
 
-//Get the default connection
+// La variable mongoose nous permettra d'utiliser les fonctionnalités du module mongoose.
+var mongoose = require("mongoose");
+// Ces options sont recommandées par mLab pour une connexion à la base
+var options = {
+  server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+};
+
+//URL de notre base
+var urlmongo = "mongodb://localhost:27017/projetMP";
+
+// Nous connectons l'API à notre base de données
+mongoose.connect(urlmongo);
+
 var db = mongoose.connection;
+db.on("error", console.error.bind(console, "Erreur lors de la connexion"));
+db.once("open", function () {
+  console.log("Connexion à la base OK");
+});
 
-//Bind connection to error event (to get notification of connection errors)
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// Nous créons un objet de type Express.
+var app = express();
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
